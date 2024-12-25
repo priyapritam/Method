00ab59d02489dec5473c082a335240c8aa4b0db4
priya <priya@PRIYAPRITAM>
2024-12-25 12:09:58 +0530

    Method Commit

---
+++ src/com/jsp/pritam/method/ConcreteMethod.java
@@ -0,0 +1,15 @@
+package com.jsp.pritam.method;
+
+public class ConcreteMethod {
+
+	/*Concrete Method ->
+	 * Concrete Method is a complete method which has both the declaration as well as the method implementation.
+	 */
+	
+	//Method Declaration
+	void start()
+	{
+		// Method Implementation/logic/body/definition
+		System.out.println("Hello Pritam!!");
+	}
+}
